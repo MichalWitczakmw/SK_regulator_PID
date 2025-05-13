3b77ef896186153f2342b42d6f815e1d6dd95558
Konrad <115174801+KonradSzkolka@users.noreply.github.com>
2025-05-13 20:54:54 +0200

    niew wiem

--- networkclient.cpp
+++ networkclient.cpp
@@ -8,55 +8,43 @@ MyTCPClient::MyTCPClient(QObject *parent)
     connect(&m_socket,SIGNAL(readyRead()),this,SLOT(slot_readyRead()));
 
     connect(&m_socket,SIGNAL(disconnected()),this,SIGNAL(disconnected()));
-
-    //connect(&m_socket, &QTcpSocket::readyRead, this, &MyTCPClient::slot_gotData);
 }
+void MyTCPClient::slot_readyRead()
+{
+    m_buffer.append(m_socket.readAll());
 
-// void MyTCPClient::slot_gotData()
-// {
-//     QDataStream in(&m_socket);
-//     in.setVersion(QDataStream::Qt_5_15);  // lub inna wersja zgodna z Twoją aplikacją
+    while (m_buffer.size() >= 8) {
+        QByteArray typeBytes = m_buffer.mid(0, 4); // Typ wiadomości (np. "FRAM")
+        QString type(typeBytes);
 
-//     while (true)
-//     {
-//         if (m_socket.bytesAvailable() < 8)
-//             return; // za mało danych, czekaj dalej
+        QDataStream lengthStream(m_buffer.mid(4, 4)); // Rozmiar danych
+        quint32 payloadSize;
+        lengthStream >> payloadSize;
 
-//         char type[4];
-//         in.readRawData(type, 4);
+        if (m_buffer.size() < 8 + payloadSize)
+            return; // Poczekaj na więcej danych
 
-//         quint32 size;
-//         in >> size;
+        QByteArray payload = m_buffer.mid(8, payloadSize);
+        m_buffer.remove(0, 8 + payloadSize);
 
-//         if (m_socket.bytesAvailable() < size)
-//         {
-//             // czekaj aż cały payload dotrze
-//             m_socket.seek(m_socket.pos() - 8); // cofnij, bo niepełne dane
-//             return;
-//         }
+        if (type == "FRAM") {
+            QDataStream in(&payload, QIODevice::ReadOnly);
+            SimulationFrame frame;
+            in >> frame; // Deserializacja ramki
 
-//         QByteArray payload;
-//         payload.resize(size);
-//         in.readRawData(payload.data(), size);
+            // Przetwarzanie ramki
+            qDebug() << "Odebrano ramkę symulacji - Tick:" << frame.tick;
+            qDebug() << "Generator Output:" << frame.geneartor_output;
+            qDebug() << "PID Output:" << frame.pid_output;
+            qDebug() << "Error:" << frame.error;
 
-//         QString typ(QString::fromLatin1(type, 4));
-
-//         qDebug() << "Odebrano typ:" << typ << " payload:" << payload;
-
-//         if (typ == "CMD_") {
-//             QString komenda = QString::fromUtf8(payload);
-//             if (komenda == "START") {
-//                 qDebug() << "Klient: odebrano START";
-//                 emit startReceived();
-//             }
-//         }
-//         else if (typ == "FRAM") {
-//             qDebug() << "Klient: odebrano rameczkę ->" << payload.toHex();
-//             emit newFrameReceived(payload);
-//         }
-//     }
-// }
-
+            // Dodaj logikę wykorzystania danych (np. aktualizacja UI, zapis do pliku itp.)
+            Simulation::get_instance().receiveFrameFromServer(frame);
+        } else {
+            qDebug() << "Nieznany typ wiadomości:" << type;
+        }
+    }
+}
 void MyTCPClient::sendFrame(const SimulationFrame &frame)
 {
     QByteArray buffer;
@@ -102,49 +90,6 @@ void MyTCPClient::slot_socket_disconnected()
     emit serverDisconnected();
 }
 
-
-void MyTCPClient::slot_readyRead()
-{
-    m_buffer.append(m_socket.readAll());
-
-    while (m_buffer.size() >= 8) {
-        QByteArray typeBytes = m_buffer.mid(0, 4);
-        QString type(typeBytes);
-
-        QDataStream lengthStream(m_buffer.mid(4, 4));
-
-        quint32 payloadSize;
-        lengthStream >> payloadSize;
-
-        if (m_buffer.size() < 8 + payloadSize)
-            return; // Poczekaj na więcej danych
-
-        QByteArray payload = m_buffer.mid(8, payloadSize);
-        m_buffer.remove(0, 8 + payloadSize);
-
-        if (type == "CMD_") {
-            QString command = QString::fromUtf8(payload);
-            qDebug() << "Command received:" << command;
-            if (command == "START") Simulation::get_instance().start();
-            else if (command == "STOP") Simulation::get_instance().stop();
-            else if (command == "RESET") Simulation::get_instance().reset();
-        } else if (type == "FRAM") {
-            QDataStream in(&payload, QIODevice::ReadOnly);
-
-            SimulationFrame frame;
-            in >> frame;
-            qDebug() << "Simulation frame received - Tick:" << frame.tick;
-            Simulation::get_instance().receiveFrameFromServer(frame);
-        }
-    }
-
-    // // Obsługa rozłączenia
-    // if (message == "Server disconnected") {
-    //     qDebug() << "Server disconnected";
-    //     emit serverDisconnected();
-    //     return;
-    // }
-}
 
 // Poprawiona funkcja sprawdzająca faktyczne połączenie
 bool MyTCPClient::isConnected() const
