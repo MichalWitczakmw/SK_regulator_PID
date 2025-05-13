b958f0cc7a93f060c732c8d134ca51b8393ae55e
Konrad <115174801+KonradSzkolka@users.noreply.github.com>
2025-05-13 17:03:28 +0200

    usunięto plik build

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_regulatixPRO_FILE C:/Users/szkol/Documents/GitHub/SK_regulator_PID/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/regulatixPRO.exe)
-set(__QT_DEPLOY_TARGET_regulatixPRO_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_regulatixPRO_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
